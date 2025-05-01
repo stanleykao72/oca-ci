7fa35aac45ea8495c20c42218b2971e29bb41293
Stanley Kao <stanleykao72@gmail.com>
2025-05-01 13:58:21 +0800

    ubuntu 24.04 and python 3.12.3

--- create_secret.sh
+++ create_secret.sh
@@ -1,3 +1,6 @@
+kubectl delete secret gh-docker-credentials
+# 後產生或更新 Kubernetes 的 docker 憑證 Secret
 kubectl create secret generic gh-docker-credentials \
   --from-file=.dockerconfigjson=./.dockerconfigjson \
-  --type=kubernetes.io/dockerconfigjson
+  --type=kubernetes.io/dockerconfigjson \
+  --dry-run=client -o yaml | kubectl apply -f -
